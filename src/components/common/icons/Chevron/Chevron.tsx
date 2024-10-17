a6948b8ee16d15b953de4424f56a6a41dcc1d4ed
DanielSimonsen90 <danielsimonsen90@gmail.com>
2024-10-17 16:54:18 +0200

    Make tabbar collapsible

--- src/components/common/icons/Chevron/Chevron.tsx
+++ src/components/common/icons/Chevron/Chevron.tsx
@@ -2,11 +2,15 @@ import { classNames } from "@/common/functions/strings";
 
 type Props = {
   point: 'up' | 'down' | 'left' | 'right';
+  onClick?: () => void;
 }
 
-export default function Chevron({ point }: Props) {
+export default function Chevron({ point, onClick }: Props) {
   return (
-    <svg className={classNames('icon', 'chevron', `chevron--${point}`)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <svg className={classNames('icon', 'chevron', `chevron--${point}`)} onClick={onClick}
+      xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" 
+      fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+    >
       <polyline points="15 18 9 12 15 6"></polyline>
     </svg>
   );
