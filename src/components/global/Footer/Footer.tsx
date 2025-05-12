cfb3a968e6b1e66ce5162c43a81dc0ac34c4e1ab
DanielSimonsen90 <danielsimonsen90@gmail.com>
2025-05-12 12:39:44 +0200

    Disclaim sources

--- src/components/global/Footer/Footer.tsx
+++ src/components/global/Footer/Footer.tsx
@@ -1,10 +1,30 @@
 import { DEVELOPER, DEVELOPER_GITHUB_URL, PROJECT_GITHUB_URL, DOMAIN_NAME } from '@/common/constants/domain';
 
 export default function Footer() {
+  const sources = [
+    { name: 'Hoyoverse', url: 'https://genshin.hoyoverse.com/en/' },
+    { name: 'Paimon.moe', url: 'https://paimon.moe/' },
+    { name: 'LustonPull', url: 'https://lustonpull.com/' },
+    // { name: 'Rerollcdn', url: 'https://rerollcdn.com/' },
+  ]
+  
   return (
     <footer className='site-footer'>
       <p>Developed by <a href={DEVELOPER_GITHUB_URL} target="_blank" rel="noopener noreferrer">{DEVELOPER}</a></p>
-      <p>&copy; {new Date().getFullYear()} {DOMAIN_NAME}. All rights reserved.</p>
+      <p className='disclaimer muted'>
+        <span>
+          Source data from {sources.map((source, index) => (
+            <span key={source.name}>
+              <a href={source.url} target="_blank" rel="noopener noreferrer">{source.name}</a>
+              {index < sources.length - 2 ? ',' : ''}
+              {index === sources.length - 2 ? ' &' : ''}
+            </span>
+          ))}
+        </span>
+        <span>
+          The software is not affiliated with any of the sources.
+        </span>
+      </p>
       <p>View on <a href={PROJECT_GITHUB_URL} target="_blank" rel="noopener noreferrer">GitHub</a></p>
     </footer>
   );
--- src/services/ImageService.ts
+++ src/services/ImageService.ts
@@ -6,7 +6,7 @@ import type * as DomainsData from '@/data/domains';
 import BaseService from './BaseService';
 
 const PAIMON_MOE_URL = 'https://paimon.moe/images';
-const REROLL_CDN_URL = 'https://rerollcdn.com/GENSHIN';
+// const REROLL_CDN_URL = 'https://rerollcdn.com/GENSHIN'; -- Archived replaced by LustonPull
 const LUSTON_PULL_CDN_URL = 'https://lustonpull.com/GENSHIN'
 const LOCAL_PATH = '../assets/images';
 
@@ -14,7 +14,7 @@ export const ImageService = new class ImageService extends BaseService<string> {
   public getArtifactImage(set: keyof typeof ArtifactSetData | string, part: ArtifactPartName): string {
     return this.lastResult = set.includes('Prayers')
       ? `${PAIMON_MOE_URL}/artifacts/${snakeCaseFromCamelCase(set).replace("'", '')}_${part === 'Feather' ? 'plume' : snakeCaseFromCamelCase(part)}.png`
-      : `${REROLL_CDN_URL}/Gear/${snakeCaseFromCamelCase(set)}.png`;
+      : `${LUSTON_PULL_CDN_URL}/Gear/${snakeCaseFromCamelCase(set)}.png`;
   }
   
   public getCharacterImage(name: keyof typeof CharacterData | string): string {
@@ -39,14 +39,14 @@ export const ImageService = new class ImageService extends BaseService<string> {
   }
 
   public getElementImage(name: Element): string {
-    return this.lastResult = `${REROLL_CDN_URL}/Elements/Element_${this.formatRerollCdnName(name)}.png`;
+    return this.lastResult = `${LUSTON_PULL_CDN_URL}/Elements/Element_${this.formatRerollCdnName(name)}.png`;
   }
   
   public getWeaponTypeImage(name: Weapon): string {
-    return this.lastResult = `${REROLL_CDN_URL}/UI/weapon_${snakeCaseFromCamelCase(name)}.png`;
+    return this.lastResult = `${LUSTON_PULL_CDN_URL}/UI/weapon_${snakeCaseFromCamelCase(name)}.png`;
   }
   public getWeaponImage(name: string): string {
-    return this.lastResult = `${REROLL_CDN_URL}/Weapons/${this.formatRerollCdnName(name)}.png`;
+    return this.lastResult = `${LUSTON_PULL_CDN_URL}/Weapons/${this.formatRerollCdnName(name)}.png`;
   }
 
   public getDomainImage(name: keyof typeof DomainsData | string): string {
