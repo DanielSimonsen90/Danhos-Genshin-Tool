e0d501cf442aad218cb0182618dc606e77df26ff
DanielSimonsen90 <danielsimonsen90@gmail.com>
2025-08-25 19:55:04 +0200

    Fix duplicate GlobalProvider usage

--- src/pages/_Layout/Layout.tsx
+++ src/pages/_Layout/Layout.tsx
@@ -3,19 +3,16 @@ import { Outlet } from "react-router-dom";
 import Header from "@/components/global/Header";
 import Footer from "@/components/global/Footer";
 
-import GlobalProvider from "@/providers";
 import SettingsLayer from "@/components/global/SettingsLayer";
 
 export default function Layout() {
   return (
-    <GlobalProvider>
-      <SettingsLayer>
-        <div id="app">
-          <Header />
-          <Outlet />
-          <Footer />
-        </div>
-      </SettingsLayer>
-    </GlobalProvider>
+    <SettingsLayer>
+      <div id="app">
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    </SettingsLayer>
   );
 }
